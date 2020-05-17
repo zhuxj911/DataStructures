7767391c73e4b1c4907cf09aec91d7612061d265
xa zhuxj <xazhuxj@qq.com>
2020-05-17 23:29:42 +0800

    完成了shell排序 version 5！

--- src/com/xazhuxj/sort/Sort.java
+++ src/com/xazhuxj/sort/Sort.java
@@ -35,9 +35,7 @@ public class Sort {
      * @param array
      */
     public static void selectSort(int[] array){
-        int temp = 0;
-        int minValue;
-        int minIndex;
+        int minValue, minIndex;
 
         for(int i=0; i<array.length-1; i++) {
             minValue = array[i];
@@ -53,7 +51,78 @@ public class Sort {
                 array[minIndex] = array[i];
                 array[i] = minValue;
             }
-            System.out.println(Arrays.toString(array));
+//            System.out.println(Arrays.toString(array));
+        }
+    }
+
+    /**
+     * 插入排序 O(n^2)
+     * 思想
+     * 一个有序数组+无序数组， 无序数组向有序数组中插值
+     * 对极端情况 【1 2 3 4 5 6 1】，效率不高，可改用shell排序
+     * @param array
+     */
+    public static void insertSort(int[] array){
+        int insertValue, insertIndex;
+
+        for(int i=1; i<array.length; i++) {
+            insertValue = array[i]; //待插入的初始值
+            insertIndex = i-1;      //待插入的位置
+
+            //给insertValue找到插入的位置
+            //说明
+            //1. insertIndex >=0 保证不越界
+            //2. 当 insertValue < array[insertIndex]， 待插入的数，还没有找到插入位置
+            //3. 此时需要将 array[insertIndex] 后移
+            while(insertIndex >=0 && insertValue < array[insertIndex]){
+                array[insertIndex+1] = array[insertIndex];
+                insertIndex--;
+            }
+
+            //当退出while循环时，说明插入的位置找到， insertIndex+1
+            //if(insertIndex+1 != i) //该句判断是否需要
+            array[insertIndex+1] = insertValue;
+//            System.out.println(Arrays.toString(array));
+        }
+    }
+//    这种方法不可行
+//    public static void shellSort0(int[] arr) {
+//        int temp;
+//        for (int step = arr.length / 2; step > 0; step /= 2) {
+//            for (int i = step; i < arr.length; i++) {
+//                for (int j = i - step; j >= 0; j -= step) {
+//                    if (arr[j] > arr[j + step]) {
+//                        temp = arr[j];
+//                        arr[j] = arr[j + step];
+//                        arr[j + step] = temp;
+//                    }
+//                }
+//            }
+////            System.out.println("After shell sort :" + Arrays.toString(arr));
+//        }
+//    }
+
+    public static void shellSort(int[] arr) {
+        int temp, j;
+        // 增量gap,并逐步的缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            // 从第step个元素，逐个对其所在的组进行插入排序
+            for (int i = gap; i < arr.length; i++) {
+                j = i;
+                temp = arr[j];
+                if (arr[j] < arr[j - gap]) {
+                    while (j - gap >= 0 && temp < arr[j - gap]) {
+                        // 移动
+                        arr[j] = arr[j - gap];
+                        j -= gap;
+                    }
+                    // 当退出while后，就给temp找到插入的位置
+                    arr[j] = temp;
+                }
+            }
+//			System.out.println("After shell sort :" + Arrays.toString(arr));
         }
     }
 }
+
+
--- src/com/xazhuxj/sort/SortDemo.java
+++ src/com/xazhuxj/sort/SortDemo.java
@@ -8,8 +8,7 @@ public class SortDemo {
     public static void main(String[] args) {
 //        int[] arr = {2, 8, -1, 3, -5};
 //        int[] arr = {1, 2, 3, 4, 5};
-//        int max = 80000;
-        int max = 5;
+        int max = 100000;
 
         int[] arr = new int[max];
         for (int i = 0; i <max ; i++) {
@@ -21,8 +20,11 @@ public class SortDemo {
         String date1Str = simpleDateFormat.format(date1);
         System.out.println("排序前的时间：" + date1Str);
 
-        //Sort.bubbleSort(arr);
-        Sort.selectSort(arr);
+//        Sort.bubbleSort(arr); //10万个数据，大约在20秒左右
+//        Sort.selectSort(arr);   //10万个数据，大约在4~5秒左右
+//        Sort.insertSort(arr);   //10万个数据，大约在1~2秒左右
+//        Sort.shellSort0(arr);   //交换法 10万个数据，插入法大约在9~10秒左右
+        Sort.shellSort(arr);   //移位法 10万个数据，插入法大约在1秒左右
 
         Date date2 = new Date();
         String date2Str = simpleDateFormat.format(date2);
